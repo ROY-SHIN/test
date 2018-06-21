aeefe824fb1ae227e0e0dc9f589f3c2fb290ba6e
roy <roy_shin@keti.re.kr>
2018-06-21 15:59:02 +0900

    1~5000까지의 self_number 합

--- coding_ex36p.py
+++ coding_ex36p.py
@@ -1,9 +1,11 @@
 num=[]
 etc=[]
-for a in range(0, 10):
+for a in range(0, 6):
     for b in range(0, 10):
-        num.append(11*a+2*b)
-        etc.append(10*a+b)
+        for c in range(0, 10):
+            for d in range(0,10):
+                num.append(1001*a+101*b+11*c+2*d)
+                etc.append(1000*a+100*b+10*c+d)
 
 self_number = set(etc)-set(num)
 print(sum(self_number))
